7c6a7d85c87902402fe3564bd68f68281a2dc629
zar4za <zar4z4@yandex.ru>
2023-02-21 11:44:30 +0300

    feat: add/getsum of events
    Реализован контроллер событий, в котором можно запостить события и получить сумму событий за последнюю минуту. Контроллер хранит события в статическом объекте.

---
+++ Pryaniki/Controllers/EventController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pryaniki.Models;
+using Pryaniki.Services;
+using Pryaniki.Utility;
+
+namespace Pryaniki.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private static readonly StorageService _storage = new();
+
+        [HttpPost]
+        public IActionResult Post(Event action) 
+        {
+            _storage.AddEvent(action);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var time = DateTime.UtcNow.RoundToMinutes();
+            var sum = _storage.Events
+                .Where(e => e.DateTime >= time)
+                .Sum(e => e.Value);
+
+            return Ok(new
+            {
+                time = time.ToString("dd.MM.yyyy HH:mm:ss"),
+                sum
+            });
+        }
+    }
+}
---
+++ Pryaniki/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Pryaniki.Models
+{
+    public class Event
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = null!;
+
+        [JsonPropertyName("value")]
+        public int Value { get; set; }
+
+        [JsonIgnore]
+        public DateTime DateTime { get; set; }
+    }
+}
