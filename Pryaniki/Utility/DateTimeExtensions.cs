89065b73f463b59571853242ff853ba19b349ff4
zar4za <zar4z4@yandex.ru>
2023-02-21 11:41:37 +0300

    feat: datetime round to minute
    Метод расширения для DateTime, который округляет время до минут.

---
+++ Pryaniki/Utility/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Pryaniki.Utility
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime RoundToMinutes(this DateTime dateTime)
+        {
+            return dateTime - new TimeSpan(0, 0, dateTime.Second);
+        }
+    }
+}
