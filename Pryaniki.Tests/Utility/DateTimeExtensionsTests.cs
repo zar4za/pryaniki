fb4114e1a696d252a936f6646dada950093c7d5a
zar4za <zar4z4@yandex.ru>
2023-02-21 11:55:25 +0300

    test: datetime extentions tests
    Тесты для DateTimeExtentions.

---
+++ Pryaniki.Tests/Utility/DateTimeExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using Pryaniki.Utility;
+
+namespace Pryaniki.Tests.Utility
+{
+    public class DateTimeExtensionsTests
+    {
+        [Fact]
+        public void RoundToMinutes_DateTimeZeroSeconds_ShouldRoundCorrectly()
+        {
+            var actual = new DateTime(2023, 2, 21, 11, 51, 00);
+            var expected = new DateTime(2023, 2, 21, 11, 51, 00);
+
+            var round = actual.RoundToMinutes();
+
+            Assert.Equal(expected, round);
+        }
+
+        [Fact]
+        public void RoundToMinutes_DateTimeFiftyNineSeconds_ShouldRoundCorrectly()
+        {
+            var actual = new DateTime(2023, 2, 21, 11, 51, 59);
+            var expected = new DateTime(2023, 2, 21, 11, 51, 00);
+
+            var round = actual.RoundToMinutes();
+
+            Assert.Equal(expected, round);
+        }
+    }
+}
